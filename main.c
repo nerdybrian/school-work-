052f5717f4bd8b4485afe532f55feec1481f7b27
nerdybrian <136691291+nerdybrian@users.noreply.github.com>
2023-10-16 21:22:10 +0300

    z

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void)
+{
+    int a, b, c , d;
+    printf("enter the first value: ");
+    scanf("%d" , &a);
+    printf("enter the second value: ");
+    scanf("%d", &b);
+    printf("enter the third value: ");
+    scanf("%d", &c);
+
+    d = a + b + c ;
+
+    printf("%d + %d + %d = %d\n", a, b, c, d);
+    return 0;
+}
